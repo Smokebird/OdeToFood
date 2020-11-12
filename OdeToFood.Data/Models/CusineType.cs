e58ccd3d616081267d929903ea17889d77a7ea44
Emily Platts <eplatts14@gmail.com>
2020-11-12 15:17:32 -0600

    Add Autofac

---
+++ OdeToFood.Data/Models/CusineType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    public enum CusineType
+    {
+        None = 0,
+        Italian = 1,
+        Indian = 2,
+        French = 3
+    }
+}
