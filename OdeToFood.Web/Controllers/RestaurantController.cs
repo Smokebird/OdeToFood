b3580eaaa86479e3be4da4a483a2969a010c8e30
Emily Platts <eplatts14@gmail.com>
2020-11-18 07:26:46 -0600

    add resaurants page

---
+++ OdeToFood.Web/Controllers/RestaurantController.cs
@@ -0,0 +1,22 @@
+﻿using OdeToFood.Data.Services;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private readonly IRestaurantData _db;
+
+        public RestaurantController(IRestaurantData db)
+        {
+            _db = db;
+        }
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            var model = _db.GetAll();
+            return View(model);
+        }
+    }
+}
